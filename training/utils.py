46ffb6f0b6410d21692c7d0caf253020b86d9fae
Sou Yoshihara <30688238+sousquared@users.noreply.github.com>
2020-12-22 16:40:31 +0900

    Update utils.py

--- training/utils.py
+++ training/utils.py
@@ -1,6 +1,7 @@
 import shutil
 
 import torch
+from torchvision import models
 
 import numpy as np
 import cv2
@@ -140,6 +141,23 @@ def save_checkpoint(state, is_best, param_path, epoch):
 def save_model(state, param_path, epoch):
     filename = param_path + 'epoch_{}.pth.tar'.format(epoch)
     torch.save(state, filename)
+
+
+def load_model(model_path, arch='alexnet'):
+    """Load model. 
+    Args:
+        model_path: Path to the pytorch saved file of the model you want to use
+        arch: Architecture of CNN
+    Returns: CNN model 
+    """
+    checkpoint = torch.load(model_path, map_location='cuda:0')
+    model = models.__dict__[arch]()
+    try:
+        model.load_state_dict(checkpoint['state_dict'])
+    except RuntimeError:
+        model.features = torch.nn.DataParallel(model.features)
+        model.load_state_dict(checkpoint['state_dict'])
+    return model
 
 
 def print_settings(model, args):
