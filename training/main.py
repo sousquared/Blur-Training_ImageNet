1612a914003cc8f757dda43231d1e78eebdf0573
Sou Yoshihara <30688238+sousquared@users.noreply.github.com>
2021-01-08 16:39:06 +0900

    Update main.py

--- training/main.py
+++ training/main.py
@@ -9,8 +9,11 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
+import torch.distributed as dist
 import torch.optim
+import torch.multiprocessing as mp
 import torch.utils.data
+import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
@@ -21,7 +24,7 @@ from utils import *
 
 
 ###################################################################
-# TO DO: set path to ImageNet
+# TODO: set path to ImageNet
 # This directory needs to have 'train' and 'val' subdirectories.
 IMAGENET_PATH = '/mnt/data/ImageNet/ILSVRC2012/'
 ###################################################################
@@ -45,7 +48,9 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
-                    help='mini-batch size (default: 256)')
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -61,16 +66,29 @@ parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_true',
                     help='use pre-trained model')
+parser.add_argument('--world-size', default=-1, type=int,
+                    help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
 parser.add_argument('--seed', default=42, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
+parser.add_argument('--multiprocessing-distributed', action='store_true',
+                    help='Use multi-processing distributed training to launch '
+                         'N processes per node, which has N GPUs. This is the '
+                         'fastest way to use PyTorch for either single node or '
+                         'multi node data parallel training')
 ### Blur-Training additional arguments
 parser.add_argument('--exp-name', '-n', type=str, default='',
                     help='Experiment name.')
 parser.add_argument('--mode', type=str,
                     choices=['normal', 'all', 'mix',
-                             'single-step', 'reversed-single-step',
+                             'single-step', 'reversed-single-step', 'fixed-single-step',
                              'multi-steps', 'multi-steps-cbt'],
                     help='Training mode.')
 parser.add_argument('--blur-val', action='store_true', default=False,
@@ -79,6 +97,8 @@ parser.add_argument('--kernel-size', '-k', type=int, nargs=2, default=(3, 3),
                     help='Kernel size of Gaussian Blur.')
 parser.add_argument('--sigma', '-s', type=float, default=1,
                     help='Sigma of Gaussian Blur.')
+parser.add_argument('--reverse-sigma', type=float, default=1,
+                    help='Sigma of Gaussian Blur. (reversed-single-step)')
 parser.add_argument('--init-sigma', type=float, default=2,
                     help='Initial Sigma of Gaussian Blur. (multi-steps-cbt)')
 parser.add_argument('--cbt-rate', type=float, default=0.9,
@@ -115,16 +135,44 @@ def main():
                       'from checkpoints.')
         print('random seed: {}'.format(args.seed))
 
-    # call main_worker function
-    main_worker(args)
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    if args.dist_url == "env://" and args.world_size == -1:
+        args.world_size = int(os.environ["WORLD_SIZE"])
+
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+
+    ngpus_per_node = torch.cuda.device_count()
+    if args.multiprocessing_distributed:
+        # Since we have ngpus_per_node processes per node, the total world_size
+        # needs to be adjusted accordingly
+        args.world_size = ngpus_per_node * args.world_size
+        # Use torch.multiprocessing.spawn to launch distributed processes: the
+        # main_worker process function
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    else:
+        # Simply call main_worker function
+        main_worker(args.gpu, ngpus_per_node, args)
 
 
-def main_worker(args):
+def main_worker(gpu, ngpus_per_node, args):
     global best_acc1
+    args.gpu = gpu
 
     if args.gpu is not None:
         print("Use GPU: {} for training".format(args.gpu))
 
+    if args.distributed:
+        if args.dist_url == "env://" and args.rank == -1:
+            args.rank = int(os.environ["RANK"])
+        if args.multiprocessing_distributed:
+            # For multiprocessing distributed training, rank needs to be the
+            # global rank among all the processes
+            args.rank = args.rank * ngpus_per_node + gpu
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size, rank=args.rank)
     # create model
     if args.pretrained:
         print("=> using pre-trained model '{}'".format(args.arch))
@@ -133,14 +181,36 @@ def main_worker(args):
         print("=> creating model '{}'".format(args.arch))
         model = models.__dict__[args.arch]()
 
-    # set model on cuda
-    if not torch.cuda.is_available():
-        print('using CPU, this will be slow')
+    if args.distributed:
+        # For multiprocessing distributed, DistributedDataParallel constructor
+        # should always set the single device scope, otherwise,
+        # DistributedDataParallel will use all available devices.
+        if args.gpu is not None:
+            torch.cuda.set_device(args.gpu)
+            model.cuda(args.gpu)
+            # When using a single GPU per process and per
+            # DistributedDataParallel, we need to divide the batch size
+            # ourselves based on the total number of GPUs we have
+            args.batch_size = int(args.batch_size / ngpus_per_node)
+            args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        else:
+            model.cuda()
+            # DistributedDataParallel will divide and allocate batch_size to all
+            # available GPUs if device_ids are not set
+            model = torch.nn.parallel.DistributedDataParallel(model)
     elif args.gpu is not None:
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
+    elif torch.cuda.device_count() == 1:
+        model = model.cuda()
     else:
-        model.cuda()
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
@@ -191,9 +261,14 @@ def main_worker(args):
             normalize,
         ]))
 
+    if args.distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        train_sampler = None
+
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True)
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
     val_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
@@ -206,7 +281,6 @@ def main_worker(args):
         num_workers=args.workers, pin_memory=True)
 
     if args.evaluate:
-        # run only valitation
         validate(val_loader, model, criterion, args)
         return
 
@@ -217,6 +291,8 @@ def main_worker(args):
     writer = SummaryWriter(log_dir=tb_path)
 
     for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            train_sampler.set_epoch(epoch)
         adjust_learning_rate(optimizer, epoch, args)  # decay by 10 every 20 epoch
 
         # train for one epoch
@@ -243,21 +319,23 @@ def main_worker(args):
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
 
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'arch': args.arch,
-            'state_dict': model.state_dict(),
-            'best_acc1': best_acc1,
-            'optimizer': optimizer.state_dict(),
-        }, is_best, models_path, epoch + 1)
-        if (epoch + 1) % 10 == 0:  # save model every 10 epochs
-            save_model({
+        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
+                                                    and args.rank % ngpus_per_node == 0):
+            save_checkpoint({
                 'epoch': epoch + 1,
                 'arch': args.arch,
                 'state_dict': model.state_dict(),
                 'best_acc1': best_acc1,
                 'optimizer': optimizer.state_dict(),
-            }, models_path, epoch + 1)
+            }, is_best, models_path, epoch + 1)
+            if (epoch + 1) % 10 == 0:  # save model every 10 epochs
+                save_model({
+                    'epoch': epoch + 1,
+                    'arch': args.arch,
+                    'state_dict': model.state_dict(),
+                    'best_acc1': best_acc1,
+                    'optimizer': optimizer.state_dict(),
+                }, models_path, epoch + 1)
 
     writer.close()  # close tensorboardX writer
 
@@ -286,29 +364,36 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     elif args.mode == 'single-step':
         if epoch >= args.epochs // 2:
             args.sigma = 0
+    elif args.mode == 'fixed-single-step':
+        if epoch >= args.epochs // 2:
+            args.sigma = 0  # no blur
+            # fix parameters of 1st Conv layer
+            model.features[0].weight.requires_grad = False
+            model.features[0].bias.requires_grad = False
     elif args.mode == 'reversed-single-step':
         if epoch < args.epochs // 2:
             args.sigma = 0
+        else:
+            args.sigma = args.reverse_sigma
 
     end = time.time()
     for i, (images, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        # blur images
-        if args.sigma != 0:  # skip if sigma = 0 (no blur)
-            if args.mode == 'mix':
-                half1, half2 = images.chunk(2)
-                # blur first half images
-                half1 = GaussianBlurAll(half1, args.sigma)
-                images = torch.cat((half1, half2))
-            else:
-                images = GaussianBlurAll(images, args.sigma)
+        # blur images (no blur when args.sigma = 0)
+        if args.mode == 'mix':
+            half1, half2 = images.chunk(2)
+            # blur first half images
+            half1 = GaussianBlurAll(half1, args.sigma)
+            images = torch.cat((half1, half2))
+        else:
+            images = GaussianBlurAll(images, args.sigma)
 
         if torch.cuda.is_available():
             images = images.cuda(args.gpu, non_blocking=True)
             target = target.cuda(args.gpu, non_blocking=True)
-
+            
         # compute outputs
         output = model(images)
         loss = criterion(output, target)
